ea60ca87ab29de1969d55ba59ce77a86fe1027f2
Zdobdol <zdobdol@gmail.com>
2020-06-05 09:00:03 +0200

    Update contact/about/index/works and server.py Added thank_you.html

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -6,22 +6,15 @@ app = Flask(__name__)
 def my_home_function():
     return render_template('./index.html')
 
-@app.route('/index.html')
-def my_home_back_function():
-    return render_template('./index.html')
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
 
-@app.route('/about.html')
-def about():
-    return render_template('./about.html')
-
-@app.route('/works.html')
-def work():
-    return render_template('./works.html')
-
-@app.route('/contact.html')
-def contact():
-    return render_template('./contact.html')
-
-@app.route('/components.html')
-def components():
-    return render_template('./components.html')
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        print(data)
+        return redirect('/thank_you.html')
+    else:
+        return 'something went wrong! Try again'
